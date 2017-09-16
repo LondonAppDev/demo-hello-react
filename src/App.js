e459ae575b4461f55c84fedfffee88fe0be7880d
Mark Winterbottom <mark@londonappdeveloper.com>
2017-09-16 16:18:18 +0100

    Added HelloWorld component.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import HelloWorld from './HelloWorld';
+
+
 class App extends Component {
   render() {
     return (
@@ -13,7 +16,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>Testddd</p>
+        <HelloWorld name="Mark" />
       </div>
     );
   }
